6b9021428bb7421f7e4ea7bb555c374c9f4de9e4
praveen babu <rpraveenbabu851@gmail.com>
2022-05-25 11:45:19 +0530

    created scanner object, use math formulae to calcuate the length between two points, printed the length

--- Geometry.java
+++ Geometry.java
@@ -1,6 +1,10 @@
 import java.util.Scanner;
+import java.lang.Math;
 public class Geometry{
-    public static void main(String[] args){
-        
+    public static void main(String[] args){        
+        Scanner scanner = new Scanner(System.in);
+        int x1 = scanner.nextInt(), y1 = scanner.nextInt(), x2 = scanner.nextInt(), y2 = scanner.nextInt();
+        double length = Math.sqrt(Math.pow((x2 - x1), 2) - Math.pow((y2 - y1), 2) ); 
+        System.out.print("The length of the line formed by given two points is:"+ length);
     }
 }
